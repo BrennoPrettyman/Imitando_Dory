4d275db839637bb787a47f07a82034773a9cffd9
Brenno Prettyman <brennoprettyman@gmail.com>
2023-04-01 16:37:04 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Imitando_Dory;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Imitando Dory");
+        Console.WriteLine();
+
+        Console.WriteLine("Uma frase para ser traduzida: ");
+        string Baleia = Console.ReadLine()!;
+
+        string Frasebaleia = Baleia
+
+        .Replace("a", "aa")
+        .Replace("e", "ee")
+        .Replace("i", "ii")
+        .Replace("o", "oo")
+        .Replace("u", "uu");
+
+        Console.Clear();
+
+        Console.WriteLine();
+        Console.WriteLine("Frase traduzida:");
+        Console.WriteLine((Frasebaleia));
+    }
+}
